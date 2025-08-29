b72d2471d7e42d2a6710bd176b8749788ce6fcb0
mca <mca@L1-29.ssccdomain.in>
2025-08-29 14:33:43 +0530

    initial commit

---
+++ CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.test;
+import static org.junit.Assert.assertequals;
+public class CalculatorTest
+{
+Calculator calc=new Calculator();
+@test public void test addition(){assertEquals(15,calc.add(10,5)):}
+@test public void test substraction()assertEquals(5,calc.substract(10,5)):}
+@test public void test multiplication(){{assertEquals(50,calc.multiply(10,5)):}
+@test public void test division(){assertEquals(2.calc.divide(10,5)):
+}
+}
