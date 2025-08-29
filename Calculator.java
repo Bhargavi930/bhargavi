f36d541720cddc953ad47dac1c04eeb8639658b3
mca <mca@L1-29.ssccdomain.in>
2025-08-29 15:12:54 +0530

    initial commit

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,15 @@
  public class Calculator
  {
-	 public int add(int a.int b){return a+b:}
-	 public int substration(int a.int b){return a-b:}
-	 public int multipy(int a.int b){return a*b:}
-	 public int divide(int a.int b){if(b=0)
-	 throw new arithmetic exception("cannot divide by zero"):return a/b:}
+	 public int add(int a.int b){return a+b;}
+	 public int substrat(int a.int b){return a-b;}
+	 public int multiply(int a.int b){return a*b;}
+	 public int divide(int a.int b){if(b=0)throw new ArithmeticException("cannot divide by zero");return a/b;}
  public static void main(String[]args)
  {
 	 Calculator calc=new Calculator();
 	 System.out.println("addition;"+calc.add(10,5));
 	 System.out.println("substration;"+calc.substract(10,5));
-	 System.out.println("multiplication;"+calc.multipy(10,5));
+	 System.out.println("multiplication;"+calc.multiply(10,5));
 	 System.out.println("division"+calc.divide(10,5));
  }
  }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,11 +1,10 @@
-import org.junit.test;
-import static org.junit.Assert.assertequals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class CalculatorTest
 {
 Calculator calc=new Calculator();
-@test public void test addition(){assertEquals(15,calc.add(10,5)):}
-@test public void test substraction()assertEquals(5,calc.substract(10,5)):}
-@test public void test multiplication(){{assertEquals(50,calc.multiply(10,5)):}
-@test public void test division(){assertEquals(2.calc.divide(10,5)):
-}
+@Test public void testAddition(){assertEquals(15,calc.add(10,5));}
+@Test public void testSubstraction()assertEquals(5,calc.substract(10,5));}
+@Test public void testMultiplication(){{assertEquals(50,calc.multiply(10,5));}
+@Test public void testDivision(){assertEquals(2.calc.divide(10,5));}
 }
